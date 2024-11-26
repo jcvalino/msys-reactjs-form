deb198badcbbc7b11f862022bab3c4dd89119f00
jcvalino <jcvalino@multisyscorp.com>
2024-11-26 15:59:25 +0800

    :sparkles: add multiverse-ui components

---
+++ apps/docs/src/components/FormSwitch.tsx
@@ -0,0 +1,133 @@
+import { useMemo } from 'react';
+
+import { cn } from './../utilities';
+
+import FormFieldWrapper from './FormFieldWrapper';
+import Text from './Text';
+
+import './ToggleSwitch.css';
+
+type FieldError = {
+  message: string;
+};
+
+type DiscriminatedTitleProps<
+  TOnValue = true,
+  TOffValue = false,
+  TValue = TOnValue | TOffValue,
+> =
+  | {
+      title: string | ((value: NoInfer<TValue>) => string);
+      description?: string | ((value: NoInfer<TValue>) => string);
+      contained?: boolean;
+      switchPosition?: 'trailing' | 'leading';
+    }
+  | {
+      title?: never;
+      description?: never;
+      contained?: never;
+      switchPosition?: never;
+    };
+
+type DiscriminatedInlineProps<
+  TOnValue = true,
+  TOffValue = false,
+  TValue = TOnValue | TOffValue,
+> =
+  | {
+      inline: true;
+    }
+  | {
+      inline?: never;
+      label?: string | ((value: NoInfer<TValue>) => string);
+      optional?: boolean;
+      helpText?: string;
+    };
+
+type Props<
+  TOnValue = true,
+  TOffValue = false,
+  TValue = TOnValue | TOffValue,
+> = {
+  name?: string;
+  value: TValue;
+  onChange: (value: NoInfer<TValue>) => void;
+  testId?: string;
+  onValue?: TOnValue;
+  offValue?: TOffValue;
+  readOnly?: boolean;
+  error?: FieldError;
+} & DiscriminatedTitleProps<TOnValue, TOffValue, TValue> &
+  DiscriminatedInlineProps<TOnValue, TOffValue, TValue>;
+
+function FormSwitch<const TOnValue, const TOffValue>({
+  name,
+  value,
+  onChange,
+  onValue: onV,
+  offValue: offV,
+  readOnly,
+  testId,
+  title,
+  description,
+  contained = false,
+  switchPosition = 'leading',
+  error,
+  ...rest
+}: Props<TOnValue, TOffValue>) {
+  const onValue = onV ?? true;
+  const offValue = offV ?? false;
+  const isLeading = switchPosition === 'leading';
+
+  const label = useMemo(() => {
+    if (rest.inline) return undefined;
+    return typeof rest.label === 'function' ? rest.label(value) : rest.label;
+  }, [rest]);
+
+  return (
+    <FormFieldWrapper
+      name={name}
+      error={error}
+      // @ts-expect-error dynamic label
+      label={label}
+      readOnly={readOnly}
+      {...rest}
+    >
+      <div
+        className={cn(
+          'flex w-full items-center',
+          contained && 'rounded border bg-interface px-4 py-3',
+          isLeading ? 'space-x-3' : 'flex-row-reverse justify-between'
+        )}
+      >
+        <div className={cn('toggle-switch')}>
+          <input
+            id={`id-${name}`}
+            data-test-id={testId}
+            type="checkbox"
+            checked={value === onValue}
+            // @ts-expect-error dynamic value
+            onChange={(e) => onChange(e.target.checked ? onValue : offValue)}
+          />
+          <label htmlFor={`id-${name}`} />
+        </div>
+        {title ? (
+          <div className="flex flex-col space-y-1">
+            <Text weight="medium" size="body" className="text">
+              {typeof title === 'function' ? title(value) : title}
+            </Text>
+            {description && (
+              <Text weight="normal" size="caption" className="text-subtle">
+                {typeof description === 'function'
+                  ? description(value)
+                  : description}
+              </Text>
+            )}
+          </div>
+        ) : null}
+      </div>
+    </FormFieldWrapper>
+  );
+}
+
+export default FormSwitch;
---
+++ apps/docs/src/components/Tab.tsx
@@ -0,0 +1,94 @@
+import { IconType } from 'react-icons';
+import { createContext, useContext } from 'react';
+
+import { cn } from './../utilities';
+
+import Badge from './Badge';
+
+type Required<T> = {
+  [P in keyof T]-?: T[P];
+};
+const TabContext = createContext<Pick<Required<GroupProps>, 'variant'> | null>(
+  null
+);
+const useTabContext = () => {
+  const ctx = useContext(TabContext);
+  if (!ctx) throw new Error('Invalid use.');
+  return ctx;
+};
+
+const MAP_GROUP_VARIANT_CLASS = {
+  default: 'h-7 space-x-2 border-b px-2',
+  pill: 'w-min space-x-2 bg-white p-2 shadow-sm rounded-lg',
+};
+type GroupProps = {
+  variant?: keyof typeof MAP_GROUP_VARIANT_CLASS;
+  children: React.ReactElement | React.ReactElement[];
+};
+const Group = ({ variant = 'default', children }: GroupProps) => {
+  return (
+    <TabContext.Provider value={{ variant }}>
+      <ul className={cn('flex', MAP_GROUP_VARIANT_CLASS[variant])}>
+        {children}
+      </ul>
+    </TabContext.Provider>
+  );
+};
+
+const MAP_ITEM_VARIANT_CLASS = {
+  default:
+    'px-2 h-full text border-b-2 border-transparent leading-4 hover:border-brand-subtle font-medium',
+  pill: 'rounded p-2 leading-4 text-subtle hover:bg-brand-subtle hover:text-onBrand-subtle font-semibold',
+};
+
+const MAP_ACTIVE_ITEM_VARIANT_CLASS = {
+  default: 'border-brand',
+  pill: 'bg-brand-subtle text-onBrand-subtle',
+};
+
+type ItemProps = {
+  children: string;
+  isActive?: boolean;
+  onClick?: () => void;
+  icon?: IconType;
+  badge?: Pick<
+    React.ComponentProps<typeof Badge>,
+    'label' | 'variant' | 'intent'
+  >;
+};
+const Item = ({
+  children,
+  onClick,
+  badge,
+  icon: Icon,
+  isActive,
+}: ItemProps) => {
+  const { variant } = useTabContext();
+  return (
+    <li
+      className={cn(
+        'relative flex cursor-pointer items-center space-x-2 text',
+        MAP_ITEM_VARIANT_CLASS[variant],
+        isActive ? MAP_ACTIVE_ITEM_VARIANT_CLASS[variant] : ''
+      )}
+    >
+      {Icon && <Icon className="h-[0.875rem] w-[0.875rem]" />}
+      <span className="text-sm leading-4">{children}</span>
+      {badge && <Badge {...badge} />}
+      {typeof onClick === 'function' && (
+        <button
+          type="button"
+          onClick={onClick}
+          className="absolute inset-0 h-full w-full"
+        />
+      )}
+    </li>
+  );
+};
+
+const Tab = {
+  Group,
+  Item,
+};
+
+export default Tab;
